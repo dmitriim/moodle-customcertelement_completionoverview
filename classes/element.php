ee1d372e2c144e60ba0546517622a4a1117f8687
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-07-20 14:39:10 +1000

    Initial commit

---
+++ classes/element.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the customcert completion overview element core interaction API.
+ *
+ * @package    customcertelement_completionoverview
+ * @copyright  2018 Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace customcertelement_completionoverview;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The customcert completion overview element core interaction API.
+ *
+ * @package    customcertelement_completionoverview
+ * @copyright  2018 Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class element extends \mod_customcert\element {
+
+    /**
+     * This function renders the form elements when adding a customcert element.
+     *
+     * @param \mod_customcert\edit_element_form $mform the edit_form instance
+     */
+    public function render_form_elements($mform) {
+
+    }
+
+    /**
+     * Performs validation on the element values.
+     *
+     * @param array $data the submitted data
+     * @param array $files the submitted files
+     * @return array the validation errors
+     */
+    public function validate_form_elements($data, $files) {
+        // Array to return the errors.
+        $errors = array();
+
+        return $errors;
+    }
+
+    /**
+     * This will handle how form data will be saved into the data column in the
+     * customcert_elements table.
+     *
+     * @param \stdClass $data the form data
+     * @return string the json encoded array
+     */
+    public function save_unique_data($data) {
+        // Array of data we will be storing in the database.
+        $arrtostore = array(
+
+        );
+
+        return json_encode($arrtostore);
+    }
+
+    /**
+     * Handles rendering the element on the pdf.
+     *
+     * @param \pdf $pdf the pdf object
+     * @param bool $preview true if it is a preview, false otherwise
+     * @param \stdClass $user the user we are rendering this for
+     */
+    public function render($pdf, $preview, $user) {
+        global $CFG;
+
+        // If there is no element data, we have nothing to display.
+        if (empty($this->get_data())) {
+            return;
+        }
+
+    }
+
+    /**
+     * Render the element in html.
+     *
+     * This function is used to render the element when we are using the
+     * drag and drop interface to position it.
+     *
+     * @return string the html
+     */
+    public function render_html() {
+        global $PAGE, $USER;
+
+        // If there is no element data, we have nothing to display.
+        if (empty($this->get_data())) {
+            return '';
+        }
+
+
+        return '';
+    }
+
+    /**
+     * Sets the data on the form when editing an element.
+     *
+     * @param \mod_customcert\edit_element_form $mform the edit_form instance
+     */
+    public function definition_after_data($mform) {
+        parent::definition_after_data($mform);
+    }
+}
